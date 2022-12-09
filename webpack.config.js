13ab86a0c75af0b64b908889162e867f1737a516
Akamig <akamig@posteo.net>
2022-12-09 17:54:24 +0900

    FIX

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './gu.js',
-  
+  target: "web",
+  mode: "production",
   output: {
     filename: 'gu.js',
     path: path.resolve(__dirname, 'dist'),
